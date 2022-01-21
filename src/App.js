82d723708af1fb58a015b3a503c3809c02b7a509
Dhanush Shetty <dhanush.shetty@cimpress.com>
2022-01-21 22:52:05 +0530

    feat: alternate homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Root from './pages/root'
+import Home from './pages/root2'
 import Learn from './pages/learn'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route exact path={'/'} element={<Root/>} />
+          <Route exact path={'/'} element={<Home/>} />
           <Route exact path={'/learn/:id'} element={<Learn/>} />
       </Routes>
     </BrowserRouter>
